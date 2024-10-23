07808d82bdaa8656db5f12a62363a56cd4a8d0ef
saraoalmeida <sara.almeida14@hotmail.com>
2024-10-22 22:38:23 -0300

    desafio

---
+++ desafioprimeiro.js
@@ -0,0 +1,24 @@
+let nomeHeroi = "Docinho";
+let quantXp = 95000
+
+let nivelHeroi;
+
+if (quantXp < 1000) {
+    nivelHeroi = "Ferro";
+} else if (quantXp <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (quantXp <= 5000) {
+    nivelHeroi = "Prata";
+} else if (quantXp <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (quantXp <= 8000) {
+    nivelHeroi = "Platina";
+} else if (quantXp <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (quantXp <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi);
